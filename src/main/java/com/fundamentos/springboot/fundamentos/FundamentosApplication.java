5776a39a8680be48df8944866f762a4d2bc1f359
CarlaR99 <carla.rodriguez@grupoconsultel.com>
2021-11-10 14:01:08 -0400

    Dependencias dentro dependencias

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,36 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+    private ComponentDependency componentDependency; //INYECCIÓN DE DEPENDENCIA (INTERFACE)
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+
+    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency) {
+        //QUALIFIER
+        // ES PARA INDICAR A QUÉ COMPONENTE SE QUIERE REFERIRR PORQUE HAY 2 CLASES QUE IMPLEMENTAN A ESA INTERFACE
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FundamentosApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        componentDependency.saludar();
+        myBean.print();
+        myBeanWithDependency.printWithDependency();
+    }
 }
